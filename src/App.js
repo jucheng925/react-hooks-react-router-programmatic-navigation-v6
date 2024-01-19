4e64afc85fd055c3ba59d5f420d4e12b3ba8dd44
Julie Cheng <jucheng925@gmail.com>
2024-01-18 22:32:07 -0500

    practice navigating

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
-function App() {
 
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const login = () =>{
+    setIsLoggedIn(true);
+  }
+
+  const logout = () =>{
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() =>{
+    if (isLoggedIn) {
+        // navigates to Home route if user is logged in
+      navigate("/");
+    } else {
+        // navigates to Login route if user is logged out
+      navigate("/login");
+    };
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {isLoggedIn ? <NavBar logout={logout}  /> : <Navigate to="/login" />}
+      <Outlet context={login}/>
     </div>
   );
 }
